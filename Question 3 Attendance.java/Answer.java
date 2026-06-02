c6a3e4baa14d30e3d6365fe1eca3cb1a6fda59a9
Artem-Kogan <artemonko19@gmail.com>
2026-06-02 11:23:55 -0400

    Update Answer.java

--- Question 3 Attendance.java/Answer.java
+++ Question 3 Attendance.java/Answer.java
@@ -2,4 +2,16 @@ public class Attendance {
   private ArrayList<CourseRecord> historyList;
   private ArrayList<CourseRecord> mathList;
 
-  public int moreHistoryThanMathAbsences(
+  public int moreHistoryThanMathAbsences() {
+    int count = 0;
+    for (int j = 0; j < historyList.size(); j++) {
+      for (int k = 0; k < mathList.size(); k++) {
+        if (historyList.get(j).getStudentID().equals(mathList.get(k).getStudentID()) {
+          if (historyList.get(j).getAbsences() > mathList.get(k).getAbsences()) {
+            count++;
+          }
+        }
+      }
+    }
+    return count;
+  }
